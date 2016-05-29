2633d6776cad4afe93b79dbba0339c61a73cbe9b
MMarszalek <marcin.marszalek92@gmail.com>
2016-05-29 14:24:09 +0200

    Create project and update libraries

---
+++ NiewidzialnaPomoc/NiewidzialnaPomoc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NiewidzialnaPomoc.Startup))]
+namespace NiewidzialnaPomoc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
